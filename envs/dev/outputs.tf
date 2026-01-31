ea34e8d98f8cd5c9ddfb5ae06101bb026345c781
Clemouche347 <clemouchezic@gmail.com>
2026-01-31 10:10:09 +0100

    add outputs env dev

---
+++ envs/dev/outputs.tf
@@ -0,0 +1,14 @@
+output "bastion_public_ip" {
+  description = "Public IP of the bastion host"
+  value       = module.bastion.public_ip
+}
+
+output "bastion_instance_id" {
+  description = "Instance ID of the bastion"
+  value       = module.bastion.instance_id
+}
+
+output "asg_name" {
+  description = "Auto Scaling Group name"
+  value       = module.app_asg.asg_name
+}
