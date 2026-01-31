65305b8e164af20d4d22049cd454823dfdcb3bfe
Clemouche347 <clemouchezic@gmail.com>
2026-01-31 10:06:16 +0100

    add bastion outputs

--- modules/bastion/outputs.tf
+++ modules/bastion/outputs.tf
@@ -1,3 +1,17 @@
 output "bastion_public_ip" {
   value = aws_instance.bastion.public_ip
 }
+output "instance_id" {
+  description = "Bastion instance ID"
+  value       = aws_instance.bastion.id
+}
+
+output "public_ip" {
+  description = "Public IP of bastion host"
+  value       = aws_instance.bastion.public_ip
+}
+
+output "security_group_id" {
+  description = "Security group ID of bastion"
+  value       = aws_security_group.bastion.id
+}
